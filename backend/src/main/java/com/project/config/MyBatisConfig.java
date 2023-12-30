444671574e6734e9bf7c1ef7805947880f3344f0
ZONO33LHD <shunsuk.free@gmail.com>
2023-12-30 14:18:58 +0900

    :sparkles:DockerでMySQL構築

---
+++ backend/src/main/java/com/project/config/MyBatisConfig.java
@@ -0,0 +1,31 @@
+package com.project.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@MapperScan("com.project.mapper")  // マッパーインターフェースが存在するパッケージを指定
+public class MyBatisConfig {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        return factoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
